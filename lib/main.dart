49a82a3dc343c5dd69d17dcfc025f82bb9de8dcc
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-04-16 21:00:27 +0700

    SafeArea Design

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: Text("First Flutter"),
+        body: SafeArea(child: Text("First Flutter")),
       ),
     );
   }
